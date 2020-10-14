f1873ba3a6c0c144f8c354f3372b835bf86f404d
RonakFabian <ronakmanfabian@gmail.com>
2020-10-14 23:14:39 +0530

    Quest Update?

---
+++ Source/D2/Quest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Quest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class D2_API UQuest : public UObject
+{
+	GENERATED_BODY()
+	
+};
